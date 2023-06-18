0d3db3f4a378c2e8b578b13ace2aacd20971525e
joshuanwankwo <u.joshuanwankwo@gmail.com>
2023-06-18 01:52:41 +0100

    Modifies ffolder structure and adds navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,40 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { Inter } from "next/font/google";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+import Nav from "@/components/layout/nav";
+import Footer from "@/components/layout/footer";
 
 export const metadata = {
-  title: 'Miuve',
-  description: 'Migrate from AWS to Sia Cloud',
-}
+  title: "Miuve - Migrate from AWS to Sia Cloud",
+  description:
+    "Easily migrate your data from AWS to Sia  decentraliced cloud via renterd",
+  twitter: {
+    card: "summary_large_image",
+    title: "Miuve - Migrate from AWS to Sia Cloud",
+    description:
+      "Easily migrate your data from AWS to Sia  decentraliced cloud via renterd",
+    creator: "@judicodes",
+  },
+  metadataBase: new URL("https://miuve.dev"),
+  themeColor: "#FFF",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+        <Nav />
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
